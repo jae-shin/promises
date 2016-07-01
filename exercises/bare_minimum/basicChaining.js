98fe1bac64c6bbcaeeeba1196a0f629aada234e1
Jae Shin <jae5464@gmail.com>
2016-06-30 23:34:32 -0700

    Pass bare minimum tests except the last one

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,20 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var promiseConstructor = require('./promiseConstructor');
+var promisification = require('./promisification');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(user) {
+      return promisification.getGitHubProfileAsync(user);
+    }).then(function(JSONresponse) {
+      fs.writeFile(writeFilePath, JSONresponse, function(err) {
+        if (err) {
+          throw err;
+        } 
+      });
+    });
 };
 
 // Export these functions so we can test them
